2fa0af1f9e1fd1c8c1b135dcf0c5af1d44aa2831
Rouzbeh Abadi <rouzbeh.abadi@me.com>
2026-05-03 21:09:45 +0200

    Add package manifest, license, and gitignore
    Sets up an iOS only Swift package targeting iOS 13+ that depends on
    google/GoogleSignIn-iOS 7.x, under the MIT license.

---
+++ Package.swift
@@ -0,0 +1,43 @@
+// swift-tools-version: 5.9
+//
+//  Package.swift
+//  GoogleSignInKit
+//
+//  Created by Rouzbeh Abadi on 2026-05-03.
+//  Copyright (c) 2026 Rouzbeh Abadi. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "GoogleSignInKit",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "GoogleSignInKit",
+            targets: ["GoogleSignInKit"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/google/GoogleSignIn-iOS",
+            from: "7.0.0"
+        )
+    ],
+    targets: [
+        .target(
+            name: "GoogleSignInKit",
+            dependencies: [
+                .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS")
+            ],
+            path: "Sources/GoogleSignInKit"
+        ),
+        .testTarget(
+            name: "GoogleSignInKitTests",
+            dependencies: ["GoogleSignInKit"],
+            path: "Tests/GoogleSignInKitTests"
+        )
+    ]
+)
